4a99e1ba859cf5e20c502e46560dbeeba9d9399b
Sepiyan Dwi Nugroho <business.septiyan@gmail.com>
2023-03-27 12:10:06 +0700

    update domain

--- businesses/entity.go
+++ businesses/entity.go
@@ -6,7 +6,7 @@ type Business struct {
 	BusinessID    string    `json:"business_id"`
 	Business_name string    `json:"business_name"`
 	Domain        string    `json:"domain"`
-	IsActive      bool      `json:"isActive"`
+	IsActive      bool      `json:"is_active"`
 	Created_at    time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
 	Updated_at    time.Time
 }
--- categories/entity.go
+++ categories/entity.go
@@ -8,7 +8,7 @@ type Category struct {
 	CategoryID   string    `json:"category_id"`
 	CategoryName string    `json:"category_name"`
 	Slug         string    `json:"slug"`
-	IsActive     bool      `json:"isActive"`
+	IsActive     bool      `json:"is_active"`
 	BusinessID   string    `json:"business_id"`
 	Created_at   time.Time `gorm:"type:timestamp;default:CURRENT_TIMESTAMP"`
 	Updated_at   time.Time
