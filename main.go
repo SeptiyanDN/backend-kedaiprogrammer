402d07cd803ae2d52b8b7f06a10c9a6a5adc7a8c
SeptiyanDN <septiyandwinugroho02@gmail.com>
2023-04-24 00:50:27 +0700

    update cors

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func main() {
 
 	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Access-Control-Allow-Origin", "Authorization", "Content-Type", "x-requested-with"},
 		ExposeHeaders:    []string{"Content-Length"},
 		MaxAge:           12 * time.Hour,
